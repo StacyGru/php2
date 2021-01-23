e2471e4f96d9d431224ed123361b169e2c714923
StacyGru <stacy.gru@gmail.com>
2021-01-24 03:47:44 +0400

    Лаба 1, листинг 5

--- lab1/index.php
+++ lab1/index.php
@@ -16,36 +16,58 @@
 
         session_start();    // подключаем механизм сессий
 
-        if (!isset($_SESSION['user']) && isset($_POST['login']) && isset($_POST['password']) && $auth = fopen('users.csv', 'rt'))  // ЕСЛИ ПЕРЕДАНЫ ДАННЫЕ ДЛЯ ВХОДА и удалось открыть csv файл
+        if (isset($_GET['logout']))
+        {
+            session_destroy();  // удаляем инфу о пользователе
+            header('Location: index.php/'); // перенаправление на главную страницу
+            exit();
+        }
+
+        if (!isset($_SESSION['password']) && isset($_POST['login']) && isset($_POST['password']) && $auth = fopen('users.csv', 'rt'))  // ЕСЛИ ПЕРЕДАНЫ ДАННЫЕ ДЛЯ ВХОДА и удалось открыть csv файл
         {   // попытка входа
             while (!feof($auth))    // пока не достигнут конец файла, повторяем:
             {
-                $test_user = explode(',', fgets($auth)); // читаем строку и развбиваем в массив
+                $test_user = explode(';', fgets($auth)); // читаем строку и развбиваем в массив
                 if (trim($test_user[0])==$_POST['login'])   // если 1 элемент массива совпадает с введённым логином (trim - удаляет пробелы)
                 {
-                    if (isset($test_user[1]) && trim($test_user[1])==$_POST['password'])    // а 2 элемент с введённым паролем
+                    $_SESSION['login'] = $test_user[0];
+                    if ((isset($test_user[1]) && trim($test_user[1])==$_POST['password']))    // а 2 элемент с введённым паролем
                     {
-                        $_SESSION['user'] = $test_user; // пользователь найден, сохраняем данные
+                        $_SESSION['password'] = $test_user[1]; // пользователь найден, сохраняем данные
+                        header('Location: index.php/'); // перенаправление на главную страницу сайта
+                        exit(); // завершаем работу скрипта
                     }
-                    header('Location: /'); // перенаправление на главную страницу сайта
-                    exit(); // завершаем работу скрипта
-                }
+                    else
+                        break;
+                }   
             }
+            echo '<div>Неверный логин или пароль!</div>';
             fclose($auth);  // закрываем файл
         }
 
-        if(!isset($_SESSION['user']))   // ЕСЛИ ВХОД ЕЩЁ НЕ ПРОИЗОШЁЛ
+        if (!isset($_SESSION['password']))   // ЕСЛИ ВХОД ЕЩЁ НЕ ПРОИЗОШЁЛ
         {
+
             echo '<form name="auth" method="post" action="">
-            <label for="login">Введите логин:</label> <input type="text" name="login"></input><br>
-            <label for="password">Введите пароль:</label> <input type="password" name="password"></input><br>
+            <label for="login">Введите логин:</label> <input type="text" name="login"';
+            if (isset($_POST['login'])) // если логин был введён верно
+                echo ' value="'.$_POST['login'].'"';    // сохраняем значение поля
+            if (isset($_COOKIE['user_login']))  // если логин сохранён в куки
+                echo ' value="'.$_COOKIE['user_login'].'"';
+            echo '></input><br><label for="password">Введите пароль:</label> <input type="password" name="password"';
+            if (isset($_COOKIE['user_password']))   // если пароль сохранён в куки
+                echo ' value="'.$_COOKIE['user_password'].'"';
+            echo '></input><br>
             <input type="submit" value="Войти">
             </form>';
         }
         else    // ЕСЛИ ВХОД УСПЕШНО ВЫПОЛНЕН
         {
-            echo '<p>Добро пожаловать, '.$_SESSION['user'].'!</p>';
+            echo '<a href="index.php/?logout=">Выход</a>';
+            echo '<p>Добро пожаловать, '.$_SESSION['login'].'!</p>';
             include 'tree.php';
+            setcookie("user_login", $_SESSION['login'], strtotime("+30 days"));
+            setcookie("user_password", $_SESSION['password'], strtotime("+30 days"));
         }
     ?>
         </body>
