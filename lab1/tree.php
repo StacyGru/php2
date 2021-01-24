ef42e5960e816bacaf0ff477f2dc1884819a6b3f
StacyGru <stacy.gru@gmail.com>
2021-01-24 12:12:52 +0400

    Лаба 1, листинг 11 (теперь работает)

--- lab1/tree.php
+++ lab1/tree.php
@@ -3,27 +3,32 @@
     {
         $link = 'viewer.php?filename='.$path.'/'.$name; // формируем адрес ссылки
         echo '<a href=".viewer.php?filename='.UrlEncode($path). // во избежание трактовки некоторых символов как элементов URL
-            '/'.$name.'" target = "_blank">Файл '.$name.'</a>';   // выводим ссылку
+            '/'.$name.'" target = "_blank">Файл '.$name.'</a><br>';   // выводим ссылку
     }
     
     function outdirInfo($name, $path)   // передаём имя каталога и путь к нему
     {
         echo '<div>';   // начало блока
         echo 'Каталог '.$name.'<br>';   // выводим имя каталога
-
-        $dir = opendir($path);  // открываем каталог
-
-        while (($file = readdir($dir)) !== false)   // пока элементы каталога не кончились
-        {
-            if (is_dir($d)) // если элемент каталог
-                outdirInfo($d, $path.'/'.$d);   // выводим его содержимое (рекурсивная функция, повторно вызывает саму себя)
-            else if (is_file($d))   // если элемент файл
-                echo makeLink($d, $path);   // выводим его имя
-        }
-        closedir($dir);
+        // echo $path;
+        // echo count(scandir($path));
+         
+        // if ($dir = opendir($path))  // если удалось открыть каталог
+        // {
+            $dir = opendir($path);
+            $count_file = 0;
+            // echo count(scandir($path));
+            while ((($file = readdir($dir)) != false) && ($count_file < count(scandir($path))))   // пока элементы каталога не кончились
+            {
+                $count_file++;
+                if (is_dir($path.'/'.$file) && $file != '.' && $file != '..') // если элемент каталог
+                    outdirInfo($file, $path.'/'.$file);   // выводим его содержимое (рекурсивная функция, повторно вызывает саму себя)
+                else if (is_file($path.'/'.$file))   // если элемент файл
+                    echo makeLink($file, $path);   // выводим его имя
+            }
+            closedir($dir);
+        // }
         echo '</div>';  // конец блока
-
-        ///// НУЖНО ЗАЩИТИТЬ ОТ ВОЗМОЖНОСТИ БЕСКОНЕЧНОГО ВЫЗОВА ФУНКЦИЕЙ САМОЙ СЕБЯ /////
 
     }
     
@@ -33,7 +38,7 @@
     // }
 
     echo '<div id="dir_tree">'; // отображаем содержимое каталога
-    outdirInfo('','');
+    outdirInfo(basename(__DIR__), getcwd());
     echo '</div>';
 
     // ... // выводим форму для загрузки файлов
